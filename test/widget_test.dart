710e9bb63adac4e9dd43dcb4bf3b08dda68a128d
cecilia <ceciliamaria1470@gmail.com>
2022-09-20 15:25:34 -0300

    atualizacao em Teste

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,7 +14,7 @@ import 'package:app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const app());
+    await tester.pumpWidget(BytebankApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
